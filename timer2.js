a6871b40ca638d8f917fe8de0320ffd58b792833
Tharsikan <thar.navaratnam@gmail.com>
2021-11-02 22:01:42 +0000

    add into message

--- timer2.js
+++ timer2.js
@@ -1,2 +1,19 @@
+const { stdin, stdout } = require("process");
+
+const beep = () => stdout.write('\x07');
+
+const beeper = () => {
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+
+  console.log("Welcome to Interactive Timer. Press b to beep now. Press a number from 1-9 to beep after the corresponding number of seconds.")
+
+  stdin.on("data", (input) => {
+    
+  });
 
 
+
+}
+
+beeper();
