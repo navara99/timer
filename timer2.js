cd922a6f82bcd9243be0ba45dbae22ad36f6491c
Tharsikan <thar.navaratnam@gmail.com>
2021-11-02 22:05:46 +0000

    add options function

--- timer2.js
+++ timer2.js
@@ -9,11 +9,50 @@ const beeper = () => {
   console.log("Welcome to Interactive Timer. Press b to beep now. Press a number from 1-9 to beep after the corresponding number of seconds.")
 
   stdin.on("data", (input) => {
-    
+    options(input);
   });
 
+}
 
+const options = (input)=> {
+  switch (input) {
+    case "1":
 
+      break;
+    case "2":
+
+      break;
+    case "3":
+
+      break;
+    case "4":
+
+      break;
+    case "5":
+
+      break;
+    case "6":
+
+      break;
+    case "7":
+
+      break;
+    case "8":
+
+      break;
+    case "9":
+
+      break;
+    case "b":
+      beep();
+      break;
+    case "\u0003":
+      console.log("Thanks for using me, ciao!")
+      process.exit();
+    default:
+      console.log("Invalid input!");
+      break;
+  }
 }
 
 beeper();
